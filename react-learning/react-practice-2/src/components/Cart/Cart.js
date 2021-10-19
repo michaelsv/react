4377e48fcf6ae6e402f724affe041b0d8a4bccf0
michaelsv <michael.s@investing.com>
2021-10-19 11:32:46 +0300

    asdf

---
+++ react-learning/react-practice-2/src/components/Cart/Cart.js
@@ -0,0 +1,40 @@
+import {useContext} from "react";
+import classes from './Cart.module.css';
+import Modal from "../UI/Modal";
+import CartContext from "../../store/cart-context";
+import CartItem from "./CartItem";
+
+const Cart = (props) => {
+    const cartCtx = useContext(CartContext);
+    console.log(cartCtx);
+    const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+    const hasItems = cartCtx.items.length > 0;
+
+    const cartItemRemoveHandler = id => {};
+    const cartItemAddHandler = item => {};
+
+    const cartItems = <ul className={classes.cartItems}>{cartCtx.items.map(
+        item => <CartItem
+            key={item.id}
+            name={item.name}
+            amount={item.amount}
+            price={item.price}
+            onRemove={cartItemRemoveHandler.bind(null,item.id)}
+            onAdd={cartItemAddHandler.bind(null,item)}
+        />
+    )}</ul>;
+
+    return <Modal onClose={props.onClose}>
+        {cartItems}
+        <div className={classes.total}>
+            <span>Total amount:</span>
+            <span>{totalAmount}</span>
+        </div>
+        <div className={classes.actions}>
+            <button className={classes.buttonAlt} onClick={props.onClose}>Close</button>
+            {hasItems && <button className={classes.button}>Order</button>}
+        </div>
+    </Modal>;
+};
+
+export default Cart;
--- react-learning/react-practice-2/src/components/Layout/HeaderCartButton.js
+++ react-learning/react-practice-2/src/components/Layout/HeaderCartButton.js
@@ -1,14 +1,21 @@
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
+import CartContext from "../../store/cart-context";
+import {useContext} from "react";
 
 const HeaderCartButton = props => {
-    return <button className={classes.button}>
+    const cartCtx = useContext(CartContext);
+    const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+        return curNumber + item.amount;
+    }, 0);
+
+    return <button className={classes.button} onClick={props.onClick}>
         <span className={classes.icon}>
             <CartIcon />
         </span>
         <span>Your Cart</span>
         <span className={classes.badge}>
-            3
+            {numberOfCartItems}
         </span>
     </button>;
 };
